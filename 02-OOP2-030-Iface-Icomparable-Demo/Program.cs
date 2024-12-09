ad37ce7c6a9fd57601a062b6fa6f7b06a7b3493e
KOUPIL Jan <koupja@delta-skola.cz>
2024-12-09 12:06:08 +0100

    2024-12-09  Icomparable Demo

--- 02-OOP2-030-Iface-Icomparable-Demo/Program.cs
+++ 02-OOP2-030-Iface-Icomparable-Demo/Program.cs
@@ -29,10 +29,49 @@
         }
     }
 
-    class Strom
+    class Strom : IComparable
     {
         public string Druh { get; set; }
         public double Vyska { get; set; }
+
+        public int CompareTo(object? obj)
+        {
+            Strom druhyStrom = obj as Strom;
+            if (druhyStrom is null)
+                throw new ArgumentException();
+
+            return Vyska.CompareTo(druhyStrom.Vyska);
+        }
+
+        //int IComparable.CompareTo(object? obj)
+        //{
+        //    Strom druhyStrom = obj as Strom;
+        //    if (druhyStrom is null)
+        //        throw new ArgumentException();
+
+        //    return Math.Sign(this.Vyska - druhyStrom.Vyska);
+
+        //    //if (this.Vyska < druhyStrom.Vyska)
+        //    //    return -1;
+        //    //else if (this.Vyska == druhyStrom.Vyska)
+        //    //    return 0;
+        //    //else
+        //    //    return 1;
+        //}
+
+        //public int CompareTo(object? obj)
+        //{
+        //    Strom druhyStrom = obj as Strom;
+        //    if (druhyStrom is null)
+        //        throw new ArgumentException();
+
+        //    if (this.Vyska < druhyStrom.Vyska)
+        //        return -1;
+        //    else if (this.Vyska == druhyStrom.Vyska)
+        //        return 0;
+        //    else
+        //        return 1;
+        //}
     }
     
 }
