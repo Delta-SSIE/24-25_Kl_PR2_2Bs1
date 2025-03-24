2ffedbdd55ad3c3250b5edf4df853403fc000c9f
KOUPIL Jan <koupja@delta-skola.cz>
2025-03-24 13:55:56 +0100

    2025-03-24 WPF - GRID, dialog windows

---
+++ 07-WPF-04-MessageBox/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _07_WPF_04_MessageBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Máš tam chybu", "Chyba", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
+
+
+            switch (result)
+            {
+                case MessageBoxResult.OK:
+                    this.Close(); //Zavře program
+                    break;
+                case MessageBoxResult.Cancel:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ 07-WPF-06-FotoView/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _07_WPF_06_FotoView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //vyvolat dialog
+            OpenFileDialog ofd = new();
+            ofd.Title = "Select picture";
+            //ofd.Filter = "JPEG (*.jpg; *.jpeg)|*.jpg;*.jpeg";
+            ofd.Filter =
+                "All supported graphics|*.jpg;*.jpeg;*.png;*.bmp|"
+                + "JPEG (*.jpg; *.jpeg)|*.jpg;*.jpeg|"
+                + "PNG (*.png)|*.png";
+
+            if (ofd.ShowDialog() != true)
+                return; // ukončím načítání, uživatel udělal CANCEL
+
+            //co tedy vybral?
+            string filename = ofd.FileName;
+
+            //ukážu
+            FileNameTB.Text = filename;
+
+            //zkusím načíst
+            try
+            {
+                Uri imageUri = new Uri(filename);
+
+                PhotoCtrl.Source = new BitmapImage(imageUri);
+            }
+            catch
+            {
+                MessageBox.Show("Error opening file", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
